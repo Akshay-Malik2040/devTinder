53739bb24bc18235078e4534a9cbd8f85e536f7b
Akshay-Malik2040 <akshaymalik2040@gmail.com>
2025-11-03 21:56:50 +0530

    exploring get,post and delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,38 @@
 const express=require('express');
 const app=express();
 
-app.use("/server",(req,res)=>{
-    res.send("Hello from server")
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Akshay",secondName:"Malik"});
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello")
+app.post("/user",(req,res)=>{
+    console.log("save data to db");
+    res.send("data successfully saved");
 })
 
-app.use('/test',(req,res)=>{
-    res.send("just testing routes")
+app.put("/user",(req,res)=>{
+    res.send("data updated successfully using put");
 })
+
+app.patch("/user",(req,res)=>{
+    res.send("data successfully updated using patch");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("data is deleted successfully");
+})
+
+// app.use("/server",(req,res)=>{
+//     res.send("Hello from server")
+// })
+
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello hello hello")
+// })
+
+// app.use('/test',(req,res)=>{
+//     res.send("just testing routes")
+// })
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...")
