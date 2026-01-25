6fe80df4346615d585821187cf5ff455bc650602
Akshay-Malik2040 <akshaymalik2040@gmail.com>
2026-01-25 15:38:51 +0530

    implement real time chat using socket io

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -8,16 +8,21 @@ const initializeSocket=(server)=>{
     })
 
     io.on("connection",(socket)=>{
-        socket.on("joinChat",()=>{
+        socket.on("joinChat",({userId,targetUserId})=>{
+            const roomId=[userId,targetUserId].sort().join("_");
+            console.log(roomId);
+            socket.join(roomId);
 
         });
 
-        socket.on("getMessage",()=>{
-
+        socket.on("sendMessage",({firstName,userId,targetUserId,text})=>{
+            const roomId=[userId,targetUserId].sort().join("_");
+            console.log(firstName+" "+text);
+            io.to(roomId).emit("messageReceived",{firstName,text});
         })
 
         socket.on("disconnect",()=>{
-            
+
         })
     })
 }
